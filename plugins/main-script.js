a1c049352328bdac9d0b50fb67a1f4db8ba62cca
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-01-02 23:00:14 +0200

    vorterx

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -1,12 +1,11 @@
+import axios from 'axios';
 import { promises } from 'fs';
 import { join } from 'path';
-import axios from 'axios'; 
 
 let handler = async function (m, { conn, __dirname }) {
-  const githubRepoURL = 'https://github.com';
+  const githubRepoURL = 'https://github.com/salmanytofficial/XLICON-V2-MD';
 
   try {
-  
     const [, username, repoName] = githubRepoURL.match(/github\.com\/([^/]+)\/([^/]+)/);
 
     const response = await axios.get(`https://api.github.com/repos/${username}/${repoName}`);
@@ -14,28 +13,21 @@ let handler = async function (m, { conn, __dirname }) {
     if (response.status === 200) {
       const repoData = response.data;
 
-      // Format the repository information with emojis
-      const formattedInfo = `
-📂REPO IS UNDER-DEVELOPNENT ©XLICON TEAM
-MAKE SURE TO JOIN OUR GROUP : https://chat.whatsapp.com/C4ivwZKuh5bLJkqfYNPQsk
-      `.trim();
+      const info_rep = `
+📂 *REPO IS UNDER DEVELOPMENT*
 
-      // Send the formatted information as a message
-      await conn.relayMessage(m.chat,  {
-        requestPaymentMessage: {
-          currencyCodeIso4217: 'INR',
-          amount1000: 69000,
-          requestFrom: m.sender,
-          noteMessage: {
-          extendedTextMessage: {
-          text: formattedInfo,
-          contextInfo: {
-          externalAdReply: {
-          showAdAttribution: true
-          }}}}}}, {})
+- Stars: ${repoData.stargazers_count}
+- Forks: ${repoData.forks}
+- Author: ${repoData.owner.login}
+- Created at: ${repoData.created_at}
+- Last Updated: ${repoData.updated_at}
+- Repo: ${repoData.html_url}
+- [XLICON GC]: https://chat.whatsapp.com/C4ivwZKuh5bLJkqfYNPQsk
+`;
+
+      await conn.sendMessage(m.chat, info_rep 'text', { quoted: m });
     } else {
-      // Handle the case where the API request fails
-      await conn.reply(m.chat, 'Unable to fetch the Xlicon-v2 repository information.', m);
+      await conn.reply(m.chat, 'Unable to fetch the XLICON-V2 repository information.', m);
     }
   } catch (error) {
     console.error(error);
@@ -48,3 +40,4 @@ handler.tags = ['main'];
 handler.command = ['sc', 'repo', 'script'];
 
 export default handler;
+  
