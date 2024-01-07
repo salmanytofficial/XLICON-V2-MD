a59cb86a5d617fc36c016b6315d25f98af01f383
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-01-07 00:09:36 +0000

    Create channel.js

---
+++ plugins/channel.js
@@ -0,0 +1,14 @@
+let handler = async m => m.reply(`
+
+╭⭑⭑⭑★✪ *XLICON* ✪★⭑⭑⭑
+│ 📂 *BOT Name:* _XLICON-V2-MD_
+│ 📝 *Description:* _I'm XLICON-V2. A MultiDevice WhatsApp bot with rich features Created By SALMAN AMAD and Abraham Dwamena._
+│ 👤 *Owner:* _Salman Ahmad_
+│ 🌐 *Channel:* https://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e
+╰━━━━━━━━━━━━━━━━╯
+`.trim())
+handler.help = ['channel']
+handler.tags = ['main']
+handler.command = ['xchannel', 'channel' ] 
+
+export default handler
