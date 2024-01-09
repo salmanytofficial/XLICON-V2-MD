c5f9442bdfbdc6570a30564859245811424edd32
Maazin Ahamed <136259904+mznking@users.noreply.github.com>
2024-01-09 23:46:07 +0530

    ☠️ 𝚇𝙻𝙸𝙲𝙾𝙽 𝚅2 ☠️

--- config.js
+++ config.js
@@ -9,14 +9,15 @@ import axios from 'axios'
 global.owner = [
   ['233533763772', 'Abraham Dwamena', true],
   ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true], 
   ['918360234087', 'WHITE', true], 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['233533763772','923184070915','923184474176'] 
-global.prems = ['923184070915', '233533763772', '918360234087']
-global.allowed = ['923184070915', '233533763772', '918360234087']
+global.mods = ['233533763772','923184070915', '94789481495', '923184474176'] 
+global.prems = ['923184070915', '233533763772', '94789481495', '918360234087']
+global.allowed = ['923184070915', '233533763772', '94789481495', '918360234087']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
