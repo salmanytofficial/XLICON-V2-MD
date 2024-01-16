c2f95f06630b9e68868cd4edb6cac7e9916653a4
ChristianNimb <152479793+ChristianNimb@users.noreply.github.com>
2024-01-16 13:07:20 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,19 +7,19 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
+  ['231775928537', 'CBRISTIAN', true], 
+  ['2318885280591', 'CHRIS', true],
+  ['', '', ], 
+  ['', '', ],
+  ['', '', ],
   
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['923184070915', '94789481495', '923184474176','233533763772','233208179431'] 
-global.prems = ['923184070915', '233208179431', '94789481495', '918360234087','233533763772']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','233533763772']
+global.mods = ['231775928537', '231888528059', '','',''] 
+global.prems = ['231775928537', '231888528059', '', '','']
+global.allowed = ['231775928537', '231888528059', '', '','']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
