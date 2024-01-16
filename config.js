1d1b92dfbebc8ddff080f8abe2fc8fc1f7913974
ChristianNimb <152479793+ChristianNimb@users.noreply.github.com>
2024-01-16 13:13:32 +0000

    Update config.js

--- config.js
+++ config.js
@@ -56,9 +56,9 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'XLICON-V2'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
+global.packname = 'Chris' 
 global.author = '@XLICON-V2' 
-global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
+global.menuvid = 'https://i.imgur.com/y6veyQf.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
