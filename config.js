5ab76532e104982a179d8b3ed008219999e6cb8d
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-01-19 10:26:15 +0000

    Update config.js

--- config.js
+++ config.js
@@ -12,15 +12,15 @@ global.owner = [
   ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
   ['233533763772', 'Abraham Dwamena2', true],
-  ['33757054415', 'Abraham Dwamena3', true],
+  ['233531910067', 'Abraham Dwamena3', true],
   
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['923184070915', '94789481495', '923184474176','33757054415','233208179431'] 
-global.prems = ['923184070915', '233208179431', '94789481495', '918360234087','33757054415']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','33757054415']
+global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233208179431'] 
+global.prems = ['923184070915', '233208179431', '94789481495', '918360234087','233531910067'']
+global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','233531910067']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
