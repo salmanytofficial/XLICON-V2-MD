8c81d77980c1822479ea8149d25b2b2764b65b46
CoachMD1 <157785463+CoachMD1@users.noreply.github.com>
2024-01-27 10:07:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 global.owner = [
   ['918360234087', 'WHITE', true], 
   ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['2349032976727', 'Coach MD -(𝘼𝙝𝙢𝙢𝙞)', true], 
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
   ['233533763772', 'Abraham Dwamena2', true],
   ['233531910067', 'Abraham Dwamena3', true],
@@ -17,7 +17,7 @@ global.owner = [
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
-
+2349032976727
 global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233208179431'] 
 global.prems = ['923184070915','233208179431','94789481495', '918360234087','233531910067']
 global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','233531910067']
@@ -57,7 +57,7 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'XLICON-V2'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
+global.packname = 'COACH MD' 
 global.author = '@XLICON-V2' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
