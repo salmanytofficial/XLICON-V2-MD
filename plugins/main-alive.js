627f4160d8d361c185118f2e183f868cfaa81053
Husnainch773 <118224953+Husnainch773@users.noreply.github.com>
2024-01-27 13:32:06 +0400

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -5,26 +5,26 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "https://cdn.jsdelivr.net/gh/Itxmanno05/GURU-BOT@main/Assets/mp3/sajjad.m4a"
-    let url = "https://github.com/abrahamdw882"
-    let murl = "https://YouTube.com"
+    let url = "https://github.com/Husnainch773/GURU-BOT"
+    let murl = "https://youtube.com/@HusnainInformation73?si=-uEU5K9YtCXtJywb"
     let img = "https://telegra.ph/file/a03029b897f6560ad4863.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
           url: vn
         },
-        mimetype: 'audio/mp4',
+        mimetype: 'audio/mpeg',
         ptt: true,
         waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "Guru",
+        fileName: "[••𝗧𝗢𝗣 𝗟𝗘𝗚𝗘𝗡𝗗••]",
     
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
-          title: "*𓈸 𓅋⃟ 🅼︎🅡︎᭄𝐊ɪɴɢ𝄟 ≛⃝*-V2 IS ALIVE 🌟",
-          body: "*𓈸 𓅋⃟ 🅼︎🅡︎᭄𝐊ɪɴɢ𝄟 ≛⃝*",
+          title: "I AM ALIVE",
+          body: "[••𝗧𝗢𝗣 𝗟𝗘𝗚𝗘𝗡𝗗••]",
           thumbnailUrl: img,
-          sourceUrl: ,"https://chat.whatsapp.com/GaKeTqaEIgMGSWAfgj424i",
+          sourceUrl: 'https://chat.whatsapp.com/K7QfBRsk6491a6649GhvAS',
           mediaType: 1,
           renderLargerThumbnail: true
           }}
