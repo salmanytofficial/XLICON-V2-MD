c9b1e3b1ff773be6ecce0d3e75f2ce0ce65d13c3
Rohan658 <145863884+Rohan658@users.noreply.github.com>
2024-01-30 00:02:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,11 +8,11 @@ import axios from 'axios'
 
 global.owner = [
   ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['233531910067', 'Abraham Dwamena3', true],
+  ['233208179431', 'Rohan', true],
+  ['923184070915', 'Rohan Boss -(Ron)', true], 
+  ['94789481495', '𝙺𝙸𝙽𝙶 ⚡', true],
+  ['233533763772', 'Rohan2', true],
+  ['233531910067', 'Roham3', true],
   
 ] //Number of owners
 
@@ -55,21 +55,18 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'Rohan Boss'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
-global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
-global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
-global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@s4salmanyt'
-global.fgpyp = 'https://youtube.com/@s4salmanyt'
-global.fglog = 'XLICON.jpg' 
+global.packname = 'Rohan Bosss' 
+global.author = 'Rohan Boss' 
+global.menuvid = 'https://i.imgur.com/kPTptJs.jpeg'
+global.igfg = '▢✓ Follow \n' 
+global.dygp = '
+global.fglog = 'Rohan' 
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
 
-global.wait = '*🕣 _XLICON IS LODIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣Rohan boss..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✔️'
