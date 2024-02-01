5b03ef081c9479cee4ffb491b55dadd8ba487123
Sjjshdjs <150098641+Sjjshdjs@users.noreply.github.com>
2024-02-01 08:37:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['233531910067', 'Abraham Dwamena3', true],
+  ['917811937953', 'RIJU 𝙺𝙸𝙽𝙶 ⚡', true], 
+  ['917811937953', 'RIJU 𝙺𝙸𝙽𝙶 ⚡', true],
+  ['917811937953', 'RIJU 𝙺𝙸𝙽𝙶 ⚡', true], 
+  ['917811937953', 'RIJU 𝙺𝙸𝙽𝙶 ⚡', true],
+  ['917811937953', 'RIJU 𝙺𝙸𝙽𝙶 ⚡', true],
+  ['917811937953', 'RIJU 𝙺𝙸𝙽𝙶 ⚡', true],
   
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233208179431'] 
-global.prems = ['923184070915','233208179431','94789481495', '918360234087','233531910067']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','233531910067']
+global.mods = ['917811937953', '917811937953', '917811937953','917811937953','917811937953'] 
+global.prems = ['917811937953','917811937953','917811937953', '917811937953','917811937953']
+global.allowed = ['917811937953', '917811937953', '917811937953', '917811937953','917811937953']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -55,21 +55,21 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'RIJU 𝙺𝙸𝙽𝙶 ⚡'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
-global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
-global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
-global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
+global.packname = 'RIJU 𝙺𝙸𝙽𝙶 ⚡' 
+global.author = 'RIJU 𝙺𝙸𝙽𝙶 ⚡' 
+global.menuvid = 'https://telegra.ph/file/590b85877d66de6a92489.jpg'
+global.igfg = '▢✓ https://whatsapp.com/channel/0029Va5et2BL7UVOxWNBnh3Q' 
+global.dygp = 'https://Wa.me/+917811937953by/?text=RiJu☠️🙌🏻𝄞💛👀✨'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@s4salmanyt'
-global.fgpyp = 'https://youtube.com/@s4salmanyt'
-global.fglog = 'XLICON.jpg' 
+global.fgyt = 'https://youtube.com/@oyy_riju?si=JUJlg3Fdz0812XpZ'
+global.fgpyp = 'https://youtube.com/@oyy_riju?si=JUJlg3Fdz0812XpZ'
+global.fglog = 'RIJU.jpg' 
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
 
-global.wait = '*🕣 _XLICON IS LODIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _RIJU IS LODIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✔️'
