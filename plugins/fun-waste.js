93a9219019260e38c1d6fd7a60a74d2605e89107
Salman YT Official <140977479+salmanytofficial@users.noreply.github.com>
2024-02-02 20:23:36 +0500

    Update fun-waste.js

--- plugins/fun-waste.js
+++ plugins/fun-waste.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let name = conn.getName(who)
-    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
+    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './XLICON.jpg')
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/wasted', {
       avatar: pp, 
     }), 'waste.png', `*Ah! Shit :* ${name}\n\nWastedeeznuts`, m)
