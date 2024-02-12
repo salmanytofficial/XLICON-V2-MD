1eb9ae197549428f632a726371593c8cfcaec495
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-02-12 14:52:19 +0000

    Update Main-DEVS.js

--- plugins/Main-DEVS.js
+++ plugins/Main-DEVS.js
@@ -1,6 +1,7 @@
 var handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "𝐇𝐞𝐲" }, "message":{ "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-let pp = Img.getRandom()	
+  let pp = './Assets/XLICON-V2.jpg'
+
 const cat = `*_XLICON-V2_*
 
 
