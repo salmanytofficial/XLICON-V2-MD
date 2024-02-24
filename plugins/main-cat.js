b0107751510939e013ad644233f4d009e4dbf513
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-02-24 10:37:44 +0000

    Update main-cat.js

--- plugins/main-cat.js
+++ plugins/main-cat.js
@@ -13,8 +13,7 @@ let handler = async (m, {
   try {
     const start = now();
     
-    // Your code block here
-    await m.reply('https://cataas.com/cat', { caption: "*meyaoooooooooooooon!*" }, "img", m);
+    await conn.sendMessage(m.chat, 'https://cataas.com/cat', 'image', { caption: "*meyaoooooooooooooon!*" });
 
     const end = now();
     const executionTime = (end - start).toFixed(3);
@@ -23,7 +22,6 @@ let handler = async (m, {
   } catch (e) {
     console.error(`${e}\n\nCommand: cat`);
 
-    // Check if 'text' is present in the error message
     const errorMessage = e.toString().includes('text') ? e : 'An error occurred while processing the command.';
     
     await conn.sendMessage(m.chat, errorMessage, 'text', { quoted: m });
