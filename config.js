0012873c88816d55f68ab75e0242aa81b60ff1b4
ControllerTM <157882813+ControllerTM@users.noreply.github.com>
2024-03-11 09:21:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,19 +7,19 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
-  ['233208179431', 'Abraham Dwamena', true],
-  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
-  ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
-  ['233533763772', 'Abraham Dwamena2', true],
-  ['233531910067', 'Abraham Dwamena3', true],
+  ['254712817588', 'WHITE', true], 
+  ['254712817588', 'Controller_TM', true],
+  ['254712817588', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['254777741898', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
+  ['254712817588', 'Controller_TM2', true],
+  ['254712817588', 'Controller_TM3', true],
   
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233208179431'] 
-global.prems = ['923184070915','233208179431','94789481495', '918360234087','233531910067']
+global.mods = ['254712817588', '254712817588', '254777741898','254712817588','254712817588'] 
+global.prems = ['254777741898','254712817588','254712817588', '254712817588','254712817588']
 global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','233531910067']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
@@ -55,21 +55,21 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'XLICON-V2'
+global.botname = 'CONTROLLER_TM'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
-global.author = '@XLICON-V2' 
+global.packname = 'CONTROlLER TEAM' 
+global.author = '@CONTROLLER_TM' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
-global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
-global.dygp = 'https://chat.whatsapp.com/BfH0KLkICn2BjmGFMRcGMW'
-global.fgsc = 'https://github.com/salmanytofficial/XLICON-V2-MD' 
+global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.https://wa.me/254712817588' 
+global.dygp = 'https://chat.whatsapp.https://wa.me/254712817588'
+global.fgsc = 'https://github.com/ControllerTM/README.md' 
 global.fgyt = 'https://youtube.com/@s4salmanyt'
-global.fgpyp = 'https://youtube.com/@s4salmanyt'
-global.fglog = 'XLICON.jpg' 
+global.fgpyp = 'https://youtube.com/@Vdjcontroller'
+global.fglog = 'CONTROLLER.jpg' 
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
 
-global.wait = '*🕣 _XLICON IS LOADIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _CONTROLLER IS LOADIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✔️'
