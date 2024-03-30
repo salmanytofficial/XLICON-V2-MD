07f2802b1be207cec810dd5dd8a8b4066dba016b
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-03-30 16:53:30 +0000

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -47,7 +47,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
       const model = 'llama';
       const senderNumber = m.sender.replace(/[^0-9]/g, ''); 
       const session = `GURU_BOT_${senderNumber}`;
-      const guru2 = `https://ultimetron.guruapi.tech/gpt3?prompt=${prompt}`;
+      const guru2 = `https://ultimetron.guruapi.tech/gpt4?prompt=${prompt}`;
       
       let response = await fetch(guru2);
       let data = await response.json();
