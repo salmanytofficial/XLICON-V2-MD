9499efd1c9f2df035cc598dba93c4eea2131e962
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-04-27 11:43:44 +0000

    Update Gpt5.0.js

--- plugins/Gpt5.0.js
+++ plugins/Gpt5.0.js
@@ -1,34 +1,32 @@
-// NO GITA STUFF ALLOWED
 import fetch from 'node-fetch';
 
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text && !(m.quoted && m.quoted.text)) {
-  if (!text) {
-    throw `*_Need query._*\n*_Example:_* _${usedPrefix + command} When did Jesus Christ die?_`;
-  }
-     }
-  if (!text && m.quoted && m.quoted.text) {
-    text = m.quoted.text;
-  }
-
-  m.react('⚡')
+const handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
-    conn.sendPresenceUpdate('composing', m.chat);
-    
+    if (!text && !(m.quoted && m.quoted.text)) {
+      throw `*_Need query._*\n*_Example:_* _${usedPrefix + command} When did Jesus Christ die?_`;
+    }
+
+    if (!text && m.quoted && m.quoted.text) {
+      text = m.quoted.text;
+    }
+
+    m.react('⚡');
 
     const API_URL = `https://api.maher-zubair.tech/ai/bard?q=${encodeURIComponent(text)}`;
+
     const response = await fetch(API_URL);
     const data = await response.json();
 
-    m.react(done)
+    m.react('✅');
+
     if (data.status && data.data) {
       const respuestaAPI = data.data;
       conn.reply(m.chat, respuestaAPI, m);
     } else {
-      throw '*Could not get a valid answer sorry 😅.*';
+      throw '*Could not get a valid answer, sorry 😅.*';
     }
   } catch (error) {
-    throw `*oops api error. Please try again later😅.*`;
+    throw `*Oops, an API error occurred. Please try again later 😅.*`;
   }
 };
 
