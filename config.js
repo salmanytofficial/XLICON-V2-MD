7e72cf946e48da1980a5f38fd82d5b7cb6937008
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-05-12 07:16:55 +0000

    Fix syntax error

--- config.js
+++ config.js
@@ -5,33 +5,28 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 import axios from 'axios'
 
-
 global.owner = [
   ['918360234087', 'WHITE', true], 
   ['233208179431', 'Abraham Dwamena', true],
   ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
   ['233533763772', 'Abraham Dwamena2', true],
-  ['233508488192', 'Abraham Dwamena3', true],
-  
+  ['233508488192', 'Abraham Dwamena3', true]
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
-
-global.mods = ['923184070915', '94789481495', '923184474176','233531910067','233508488192'] 
-global.prems = ['923184070915','233208179431','94789481495', '918360234087','233508488192']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','233508488192']
+global.mods = ['923184070915', '94789481495', '923184474176', '233531910067', '233508488192'] 
+global.prems = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
+global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087', '233508488192']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.keysxteam = global.keysxteammm[Math.floor(global.keysxteammm.length * Math.random())]
 global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.keysneoxr = global.keysneoxrrr[Math.floor(global.keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['GataDios']
 global.beta = 'mLxstUwm'
 
 global.APIs = { // API Prefix
-  // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
   dzx: 'https://api.dhamzxploit.my.id',
   lol: 'https://api.lolhuman.xyz',
@@ -44,13 +39,13 @@ global.APIs = { // API Prefix
   bg: 'http://bochil.ddns.net',
   fgmods: 'https://api-fgmods.ddns.net'
 }
+
 global.APIKeys = { // APIKey Here
-  // 'https://website': 'apikey'
   'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,	
+  'https://api.neoxr.my.id': global.keysneoxr,
   'https://violetics.pw': 'beta',
-  'https://zenzapis.xyz': `${keysxxx}`, 
+  'https://zenzapis.xyz': global.keysxxx, 
   'https://api-fgmods.ddns.net': 'fg-dylux'
 }
 
@@ -68,8 +63,7 @@ global.fgpyp = 'https://youtube.com/@s4salmanyt'
 global.fglog = 'XLICON.jpg' 
 global.thumb = fs.readFileSync('./XLICON.jpg')
 
-
-global.wait = '*🕣 _XLICON IS LOADIND..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _XLICON IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✔️'
@@ -77,7 +71,7 @@ global.error = '❌'
 global.xmoji = '🔥' 
 
 global.multiplier = 69 
-global.maxwarn = '3' // máxima advertencias
+global.maxwarn = '3' // maximum warnings
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
@@ -85,3 +79,5 @@ watchFile(file, () => {
   console.log(chalk.redBright("Update 'config.js'"))
   import(`${file}?update=${Date.now()}`)
 })
+
+
