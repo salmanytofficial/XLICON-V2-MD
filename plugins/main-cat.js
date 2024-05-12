0f83f9b13f300e5869e12fff635e785909e2d98a
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-05-12 07:29:59 +0000

    Update main-cat.js

--- plugins/main-cat.js
+++ plugins/main-cat.js
@@ -3,29 +3,18 @@ import now from 'performance-now';
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
-let handler = async (m, {
-  conn,
-  args,
-  usedPrefix,
-  text,
-  command
-}) => {
-  try {
-    const start = now();
-    
-    await conn.sendMessage(m.chat, 'https://cataas.com/cat', 'image', { caption: "*meyaoooooooooooooon!*" });
-
-    const end = now();
-    const executionTime = (end - start).toFixed(3);
-    
-    console.log(`Execution time: ${executionTime} milliseconds`);
-  } catch (e) {
-    console.error(`${e}\n\nCommand: cat`);
-
-    const errorMessage = e.toString().includes('text') ? e : 'An error occurred while processing the command.';
-    
-    await conn.sendMessage(m.chat, errorMessage, 'text', { quoted: m });
-  }
+let handler = async (m, { conn, args, usedPrefix, text, command }) => {
+	try {
+		const start = now();
+		await conn.sendMessage(m.chat, { url: 'https://cataas.com/cat' }, 'image', { caption: "*meyaoooooooooooooon!*" });
+		const end = now();
+		const executionTime = (end - start).toFixed(3);
+		console.log(`Execution time: ${executionTime} milliseconds`);
+	} catch (e) {
+		console.error(`${e}\n\nCommand: cat`);
+		const errorMessage = e.toString().includes('text') ? e : 'An error occurred while processing the command.';
+		await conn.sendMessage(m.chat, errorMessage, 'text', { quoted: m });
+	}
 };
 
 handler.help = ['cat'];
