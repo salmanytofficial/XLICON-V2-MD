c748a02560175516cf3068bd657a282eedda662e
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-05-17 10:26:19 -0700

    Create groups-list.js

---
+++ plugins/groups-list.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn }) => {
+    let txt = ''
+    for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `${await conn.getName(jid)}\n🪪${jid} [${chat?.metadata?.read_only ? 'Left' : 'Joined'}]\n\n`
+    m.reply(`List Groups:
+${txt}
+`.trim())
+}
+handler.help = ['xglist', 'grouplists']
+handler.tags = ['info']
+handler.command = /^(group(s|list))$/i
+
+export default handler
