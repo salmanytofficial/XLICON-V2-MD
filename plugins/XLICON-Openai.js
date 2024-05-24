6fb83f305a3a0dc91219a91e6b174fd382daf73e
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-05-24 13:11:09 -0700

    Update XLICON-Openai.js

--- plugins/XLICON-Openai.js
+++ plugins/XLICON-Openai.js
@@ -1,77 +1,55 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
 let handler = async (m, { text, conn, usedPrefix, command }) => {
   if (!text && !(m.quoted && m.quoted.text)) {
-    throw `Please provide some text or quote a message to get a response.`;
+    throw `Please provide some text or quote a message to get a response.`
   }
 
   if (!text && m.quoted && m.quoted.text) {
-    text = m.quoted.text;
+    text = m.quoted.text
   }
 
   try {
     m.react(rwait)
-    const { key } = await conn.sendMessage(m.chat, {
-      image: { url: 'https://telegra.ph/file/403a47e628ef49dee27a3.jpg' },
-      caption: 'Thinking....'
-    }, {quoted: m})
-    conn.sendPresenceUpdate('composing', m.chat);
-    const prompt = encodeURIComponent(text);
-
-    const guru1 = `${gurubot}/chatgpt?text=${prompt}`;
     
+    conn.sendPresenceUpdate('composing', m.chat)
+    const prompt = encodeURIComponent(text)
+
+    const guru1 = `https://api.gurusensei.workers.dev/llama?prompt=${prompt}`
+
     try {
-      let response = await fetch(guru1);
-      let data = await response.json();
-      let result = data.result;
+      let response = await fetch(guru1)
+      let data = await response.json()
+      let result = data.response.response
 
       if (!result) {
-        
-        throw new Error('No valid JSON response from the first API');
+        throw new Error('No valid JSON response from the first API')
       }
 
-      await conn.relayMessage(m.chat, {
-        protocolMessage: {
-          key,
-          type: 14,
-          editedMessage: {
-            imageMessage: { caption: result }
-          }
-        }
-      }, {});
-      m.react(done);
+      await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/403a47e628ef49dee27a3.jpg', [['Script', `.sc`]], null, [['Fork Repo', `https://github.com/salmanytofficial/XLICON-V2-MD`]], m)
+      m.react(done)
     } catch (error) {
-      console.error('Error from the first API:', error);
+      console.error('Error from the first API:', error)
 
-  
-      const model = 'llama';
-      const senderNumber = m.sender.replace(/[^0-9]/g, ''); 
-      const session = `GURU_BOT_${senderNumber}`;
-      const guru2 = `https://ultimetron.guruapi.tech/gpt4?prompt=${prompt}`;
-      
-      let response = await fetch(guru2);
-      let data = await response.json();
-      let result = data.completion;
+      //const model = 'llama'
+     // const senderNumber = m.sender.replace(/[^0-9]/g, '')
+      //const session = `xlicon_bot_${senderNumber}`
+      const guru2 = `https://ultimetron.guruapi.tech/gpt3?prompt=${prompt}`
 
-      await conn.relayMessage(m.chat, {
-        protocolMessage: {
-          key,
-          type: 14,
-          editedMessage: {
-            imageMessage: { caption: result }
-          }
-        }
-      }, {});
-      m.react(done);
+      let response = await fetch(guru2)
+      let data = await response.json()
+      let result = data.completion
+
+     await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/403a47e628ef49dee27a3.jpg', [['Script', `.sc`]], null, [['Fork Repo', `https://github.com/salmanytofficial/XLICON-V2-MD`]], m)
+      m.react(done)
     }
-
   } catch (error) {
-    console.error('Error:', error);
-    throw `*ERROR*`;
+    console.error('Error:', error)
+    throw `*ERROR*`
   }
-};
+}
 handler.help = ['chatgpt']
 handler.tags = ['AI']
-handler.command = ['bro', 'chatgpt', 'ai', 'gpt'];
+handler.command = ['bro', 'chatgpt', 'ai', 'gpt']
 
-export default handler;
+export default handler
