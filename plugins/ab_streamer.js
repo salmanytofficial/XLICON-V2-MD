5c654d40eebb8be7e39cbb9c9f9aff17186abf59
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-05-24 20:57:13 -0700

    Update ab_streamer.js

--- plugins/ab_streamer.js
+++ plugins/ab_streamer.js
@@ -4,7 +4,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
 
 let url = 'https://telegra.ph/file/81199f8c1cdc906cf04d0.jpg'
 
-conn.sendButton(m.chat, "XLICON-V2", author, url, [['AUDIO', ${usedPrefix}ytmp3 ${text}], ['VIDEO', ${usedPrefix}ytmp4 ${text}]], null, [['AB', 'https://ab-streamer.vercel.app']], m)
+conn.sendButton(m.chat, "XLICON-V2", author, url, [['AUDIO', ${usedPrefix}ytmp3 ${text}], ['VIDEO', ${usedPrefix}ytmp4 ${text}]], null, [['AB stream', 'https://ab-streamer.vercel.app']], m)
 
   }
 
