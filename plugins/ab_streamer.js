6af03c2a3cf5e0e29e5f84393217e16145dd018b
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-05-24 20:55:52 -0700

    Create ab_streamer.js

---
+++ plugins/ab_streamer.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+let handler = async (m, { text, conn, usedPrefix, command }) => {
+
+let url = 'https://telegra.ph/file/81199f8c1cdc906cf04d0.jpg'
+
+conn.sendButton(m.chat, "XLICON-V2", author, url, [['AUDIO', ${usedPrefix}ytmp3 ${text}], ['VIDEO', ${usedPrefix}ytmp4 ${text}]], null, [['AB', 'https://ab-streamer.vercel.app']], m)
+
+  }
+
+handler.help = ['bot']
+
+handler.tags = ['img']
+
+handler.command = /^(stream)$/i
+
+export default handler
