e53ec5368744f84a19a184c914d9ca740a87384a
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-05-25 05:38:58 -0700

    Update ab_streamer.js

--- plugins/ab_streamer.js
+++ plugins/ab_streamer.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 let handler = async (m, { text, conn, usedPrefix, command }) => {
 
-let url = 'https://telegra.ph/file/81199f8c1cdc906cf04d0.jpg'
 
  await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/81199f8c1cdc906cf04d0.jpg', [['GROUPS', `${usedPrefix}groups`]], null, [['STREAM ANIME', `https://github.com/salmanytofficial/XLICON-V2-MD`]], m)
 
