ebc134ce7e76bed1d2062f008ddf25c71a20ba06
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-06-02 22:18:06 +0000

    Update ab_streamer.js

--- plugins/ab_streamer.js
+++ plugins/ab_streamer.js
@@ -1,16 +1,24 @@
-import axios from 'axios'
+import axios from 'axios';
 
 let handler = async (m, { text, conn, usedPrefix, command }) => {
+    // Define 'result' and 'author' with static values
+    let result = 'HAVE FUN WATCHING ANIME'; // Replace with your actual static message
+    let author = 'ANIME-WEB'; // Replace with the actual author name or relevant string
 
+    await conn.sendButton(
+        m.chat,
+        result,
+        author,
+        'https://telegra.ph/file/81199f8c1cdc906cf04d0.jpg',
+        [['GROUPS', `${usedPrefix}groups`]],
+        null,
+        [['STREAM ANIME', 'https://ab-streamer.vercel.app']],
+        m
+    );
+};
 
- await conn.sendButton(m.chat,result, author, 'https://telegra.ph/file/81199f8c1cdc906cf04d0.jpg', [['GROUPS', `${usedPrefix}groups`]], null, [['STREAM ANIME', `https://github.com/salmanytofficial/XLICON-V2-MD`]], m)
+handler.help = ['stream'];
+handler.tags = ['main'];
+handler.command = ['stream'];
 
-  }
-
-handler.help = ['stream']
-
-handler.tags = ['main']
-
-handler.command = ['stream']
-
-export default handler
+export default handler;
