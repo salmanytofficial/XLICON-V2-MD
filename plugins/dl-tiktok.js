be5431a4e18e0b3e4bf44b29dcf12a5b36c3541c
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-06-14 18:55:44 -0700

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,84 +1,54 @@
-import fg from 'api-dylux';
-import axios from 'axios';
-import cheerio from 'cheerio';
-import { tiktok } from '@xct007/frieren-scraper';
-import { generateWAMessageFromContent } from '@whiskeysockets/baileys';
-import { tiktokdl } from '@bochilteam/scraper';
+import 'api-dylux';
+import fetch from 'node-fetch';
 
-const CFROSAPI = global.APIs.CFROSAPI;
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  if (!args[0]) {
+    throw `✳️ Give the link of the video TikTok or quote a TikTok link\n\n 📌 Example: ${usedPrefix + command} https://vm.tiktok.com`;
+  }
+  if (!args[0].match(/tiktok/gi)) {
+    throw "❎ Please provide a valid TikTok Link";
+  }
 
-const handler = async (m, { conn, text, args, usedPrefix, command }) => {
-  if (!text) throw `_*Xlicon TIKTOK DL*_\n\n*_Paste a TikTok link._*\n\n*_Example:_* _${usedPrefix + command} Url here_`;
-  if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw `_*Xlicon TIKTOK DL*_\n\n*_Enter a TikTok link._*\n\n*_Example:_* _${usedPrefix + command} Url here_`;
-
+  m.react(rwait);
+  
   try {
-    await m.react('⏳'); // Assuming '⏳' is the wait emoji
-    const dataFn = await conn.getFile(`${CFROSAPI}/api/tiktokv2?url=${args[0]}`);
-    const desc1n = `*_|©Xlicon-MD-V2|_*`;
-    await conn.sendMessage(m.chat, { video: dataFn.data, caption: desc1n }, { quoted: m });
-    await m.react('✅'); // Assuming '✅' is the done emoji
-  } catch (err1) {
-    try {
-      const dataF = await tiktok.v1(args[0]);
-      const desc1 = `*_Here is your requested video_*`;
-      await conn.sendMessage(m.chat, { video: { url: dataF.play }, caption: desc1 }, { quoted: m });
-      await m.react('✅');
-    } catch (err2) {
-      try {
-        const tTiktok = await tiktokdlF(args[0]);
-        const desc2 = `*_Here is your requested video_*`;
-        await conn.sendMessage(m.chat, { video: { url: tTiktok.video }, caption: desc2 }, { quoted: m });
-        await m.react('✅');
-      } catch (err3) {
-        try {
-          const p = await fg.tiktok(args[0]);
-          const te = `*_Here is your requested video_*`;
-          await conn.sendMessage(m.chat, { video: { url: p.nowm }, caption: te }, { quoted: m });
-          await m.react('✅');
-        } catch (err4) {
-          try {
-            const { author: { nickname }, video, description } = await tiktokdl(args[0]);
-            const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
-            const cap = `*_Here is your requested video_*`;
-            await conn.sendMessage(m.chat, { video: { url: url }, caption: cap }, { quoted: m });
-            await m.react('✅');
-          } catch (err5) {
-            await m.react('❌'); // Assuming '❌' is the error emoji
-            throw `*_An error occurred. Please try again later._*`;
-          }
-        }
+    let response = await fetch(global.API("fgmods", '/api/downloader/tiktok', { 'url': args[0] }, "apikey"));
+    let result = await response.json();
+    
+    if (!result.result.images) {
+      let message = `
+┌─⊷ *XLICON 𝗧𝗜𝗞𝗧𝗢𝗞 𝗗𝗟* 
+┃ *Name:* ${result.result.author.nickname}
+┃ *Username:* ${result.result.author.unique_id}
+┃ *Duration:* ${result.result.duration}
+┃ *Likes:* ${result.result.digg_count}
+┃ *Views:* ${result.result.play_count}
+┃ *Description:* ${result.result.title}
+└───────────
+      `;
+      await conn.sendFile(m.chat, result.result.play, "tiktok.mp4", message, m);
+      m.react(done);
+    } else {
+      let message = `
+┌─⊷ *XLICON 𝗧𝗜𝗞𝗧𝗢𝗞 𝗗𝗟*           
+┃ *Likes:* ${result.result.digg_count}
+┃ *Description:* ${result.result.title}
+└───────────
+      `;
+      for (let image of result.result.images) {
+        await conn.sendMessage(m.chat, { image: { url: image }, caption: message }, { quoted: m });
       }
+      await conn.sendFile(m.chat, result.result.play, "tiktok.mp3", '', m, null, { mimetype: "audio/mp4" });
+      m.react(done);
     }
+  } catch (error) {
+    console.error(error);
+    m.reply("❎ Error");
   }
 };
 
-handler.command = /^(tiktok|tk|ttdl|tiktokdl|tiktoknowm|tt|ttnowm|tiktokaudio)$/i;
-handler.tags = ['downloader'];
-export default handler;
+handler.help = ["tiktok"];
+handler.tags = ['dl'];
+handler.command = ["tiktok", 'tt', "tiktokimg", 'tk'];
 
-async function tiktokdlF(url) {
-  if (!/tiktok/.test(url)) return `_*Xlicon TIKTOK DL*_\n\n*_Paste a TikTok link._*\n\n*_Example:_* _${usedPrefix + command} Url here_`;
-  const gettoken = await axios.get('https://tikdown.org/id');
-  const $ = cheerio.load(gettoken.data);
-  const token = $('#download-form > input[type=hidden]:nth-child(2)').attr('value');
-  const param = { url: url, _token: token };
-  const { data } = await axios.request('https://tikdown.org/getAjax?', {
-    method: 'post',
-    data: new URLSearchParams(Object.entries(param)),
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'
-    }
-  });
-  const getdata = cheerio.load(data.html);
-  if (data.status) {
-    return {
-      status: true,
-      thumbnail: getdata('img').attr('src'),
-      video: getdata('div.download-links > div:nth-child(1) > a').attr('href'),
-      audio: getdata('div.download-links > div:nth-child(2) > a').attr('href')
-    };
-  } else {
-    return { status: false };
-  }
-}
+export default handler;
