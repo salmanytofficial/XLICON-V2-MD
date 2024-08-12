b9bdaa2b7192cd3c66b127f19d0de9d76fac7a30
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-08-12 12:46:59 -0700

    Update XLICON-V2-Bing.js

--- plugins/XLICON-V2-Bing.js
+++ plugins/XLICON-V2-Bing.js
@@ -1,51 +1,37 @@
 import fetch from 'node-fetch';
 
-let handler = async (message, {
-  conn: connection,
-  args: arguments,
-  usedPrefix: prefix,
-  command: cmd,
-  text: inputText
-}) => {
-  if (!inputText && !(message.quoted && message.quoted.text)) {
-    throw "Please provide some text or quote a message to get a response.";
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  try {
+    if (!text) throw 'uhm.. what do you want to say?';
+    await m.react('🤖');
+
+    const prompt = encodeURIComponent(text);
+    let apiurl = `https://gpt4.guruapi.tech/bing?username=defailt&query=${prompt}`;
+
+    const result = await fetch(apiurl);
+    const response = await result.json();
+    
+    if (!response.result) throw 'No result found';
+
+    const replyText = response.result;
+    await conn.sendButton(
+      m.chat, 
+      replyText, 
+      author, 
+      'https://techcrunch.com/wp-content/uploads/2023/11/microsoft-copilot-bing.jpg', 
+      [['Script', `${usedPrefix}waifu`]], 
+      null, 
+      [['Fork repo', `https://github.com/salmanytofficial/XLICON-V2-MD`]], 
+      m
+    );
+  } catch (error) {
+    console.error(error);
+    m.reply('Oops! Something went wrong. We are trying hard to fix it ASAP.');
   }
-  if (!inputText && message.quoted && message.quoted.text) {
-    inputText = message.quoted.text;
-  }
-  message.reply("Waiting Response from Xlicon-v2 Bot server...");
-  message.react('🤔');
-  const encodedText = encodeURIComponent(inputText);
-  let response = await Bing(encodedText);
-  if (!response) {
-    throw new Error("No valid JSON response from Bing ");
-  }
-  await connection.reply(message.chat, response, message);
 };
 
-handler.help = ["bing"];
-handler.tags = ['AI'];
+handler.help = ['bing <text>'];
+handler.tags = ['tools'];
 handler.command = /^(bing)$/i;
-export default handler;
 
-async function Bing(queryText) {
-  let response = await (await fetch("https://copilot.github1s.tk/v1/chat/completions", {
-    'method': "POST",
-    'headers': {
-      'Authorization': "dummy",
-      'Content-Type': "application/json"
-    },
-    'body': JSON.stringify({
-      'model': "Creative",
-      'max_tokens': 100,
-      'messages': [{
-        'role': "system",
-        'content': "You are an helpful assistant."
-      }, {
-        'role': "user",
-        'content': queryText
-      }]
-    })
-  })).json();
-  return response.choices[0].message.content;
-}
+export default handler;
