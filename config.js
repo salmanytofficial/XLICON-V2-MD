db872de4197a1e73d4bdbeddf90c67cb4fcd44ff
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-08-22 18:21:40 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 import axios from 'axios'
 
+// Global variables
 global.owner = [
   ['918360234087', 'WHITE', true], 
   ['233208179431', 'Abraham Dwamena', true],
@@ -12,7 +13,7 @@ global.owner = [
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
   ['233533763772', 'Abraham Dwamena2', true],
   ['233268374753', 'Abraham Dwamena3', true]
-] //Number of owners
+] 
 
 global.mods = ['923184070915', '94789481495', '923184474176', '233268374753', '233508488192'] 
 global.prems = ['923184070915', '233208179431', '94789481495', '918360234087', '233268374753']
@@ -26,7 +27,7 @@ global.keysneoxr = global.keysneoxrrr[Math.floor(global.keysneoxrrr.length * Mat
 global.lolkeysapi = ['GataDios']
 global.beta = 'mLxstUwm'
 
-global.APIs = { // API Prefix
+global.APIs = {
   xteam: 'https://api.xteam.xyz', 
   dzx: 'https://api.dhamzxploit.my.id',
   lol: 'https://api.lolhuman.xyz',
@@ -40,7 +41,7 @@ global.APIs = { // API Prefix
   fgmods: 'https://api-fgmods.ddns.net'
 }
 
-global.APIKeys = { // APIKey Here
+global.APIKeys = { 
   'https://api.xteam.xyz': 'd90a9e986e18778b',
   'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
   'https://api.neoxr.my.id': global.keysneoxr,
@@ -71,7 +72,7 @@ global.error = '❌'
 global.xmoji = '🔥' 
 
 global.multiplier = 69 
-global.maxwarn = '3' // maximum warnings
+global.maxwarn = '3'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
@@ -79,5 +80,3 @@ watchFile(file, () => {
   console.log(chalk.redBright("Update 'config.js'"))
   import(`${file}?update=${Date.now()}`)
 })
-
-
