f8b667586779a5b510274d8c77950c4de26de1de
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-11-07 10:39:57 -0800

    Update antilink-delete.js

--- plugins/antilink-delete.js
+++ plugins/antilink-delete.js
@@ -1,26 +1,33 @@
-let antilinkdel = false
+let antilinkdel = false;
 
 let handler = async (m, { conn, usedPrefix, command }) => {
   if (command === 'on antilinkdel') {
-    antilinkdel = true
-    m.reply('Antilinkdel feature enabled!')
+    antilinkdel = true;
+    m.reply('Antilinkdel feature enabled!');
   } else if (command === 'off antilinkdel') {
-    antilinkdel = false
-    m.reply('Antilinkdel feature disabled!')
+    antilinkdel = false;
+    m.reply('Antilinkdel feature disabled!');
   } else if (command === 'antilinkdel' && antilinkdel) {
     m.messages.forEach(async (message) => {
       if (message.text && message.text.match(/\bhttps?:\/\/|www\./i)) {
-        conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: (link unavailable), participant: message.participant }})
+        conn.sendMessage(m.chat, {
+          delete: { 
+            remoteJid: m.chat, 
+            fromMe: false, 
+            id: message.id,  
+            participant: message.participant
+          }
+        });
       }
-    })
+    });
   }
-}
+};
 
-handler.help = ['antilink del']
-handler.tags = ['group']
-handler.command = /^(on|off)?\s*antilinkdel$/i
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
+handler.help = ['antilink del'];
+handler.tags = ['group'];
+handler.command = /^(on|off)?\s*antilinkdel$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
 
-export default handler
+export default handler;
