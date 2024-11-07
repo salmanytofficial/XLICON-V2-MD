6ffa74c7d9a4058e395c924ca789e09a0331934d
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-11-07 10:04:06 -0800

    Update button-channel.js

--- plugins/button-channel.js
+++ plugins/button-channel.js
@@ -1,10 +1,10 @@
-import fetch from 'node-fetch'
-import uploadImage from '../lib/uploadImage.js'
+import fetch from 'node-fetch';
+import uploadImage from '../lib/uploadImage.js';
 
 let handler = async m => {
-  const imageBuffer = await (await fetch('https://telegra.ph/file/403a47e628ef49dee27a3.jpg')).buffer()
-  let imgurl = await uploadImage(imageBuffer)
-  
+  const imageBuffer = await (await fetch('https://telegra.ph/file/403a47e628ef49dee27a3.jpg')).buffer();
+  let imgurl = await uploadImage(imageBuffer);
+
   const messageContent = `
   ╭⭑⭑⭑★✪ *XLICON* ✪★⭑⭑⭑
   │ 📂 *BOT Name:* _XLICON-V2-MD_
@@ -12,12 +12,19 @@ let handler = async m => {
   │ 👤 *Owner:* _Salman Ahmad_
   │ 🌐 *Channel:* https://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e
   ╰━━━━━━━━━━━━━━━━╯
-  `.trim()
-  const imageBuffer = await response.buffer()
-      let imgurl = await uploadImage(imageBuffer)
-      await conn.sendButton(m.chat,'Here is your Result', author, imgurl, [['MENU', `${usedPrefix}menu`]], null, [['WATCH ANIME', `https://ab-streamer.vercel.app`]], m)
-}
-handler.help = ['bchannel']
-handler.tags = ['main']
-handler.command = ['xlchannel', 'bchannel']
-export default handler
+  `.trim();
+
+  await conn.sendButton(
+    m.chat,
+    'Here is your Result',
+    author,
+    imgurl,
+    [['MENU', `${usedPrefix}menu`], ['WATCH ANIME', `https://ab-streamer.vercel.app`]],
+    m
+  );
+};
+
+handler.help = ['bchannel'];
+handler.tags = ['main'];
+handler.command = ['xlchannel', 'bchannel'];
+export default handler;
