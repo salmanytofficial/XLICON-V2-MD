fdddfd128d76927970b1f40263c9d9ae8559480e
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-11-07 20:08:49 -0800

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,63 +1,88 @@
-import fetch from 'node-fetch';
-import yts from 'yt-search';
-import axios from 'axios';
+import youtubedl from 'youtubedl-core';
+import 'youtube-yts';
 import fs from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import os from 'os';
+import axios from 'axios';
 
-const handler = async (m, { command, usedPrefix, conn, text }) => {
-    if (!text) throw `Please provide a song name or URL.\nExample: *${usedPrefix + command} Billie Eilish - Bellyache*`;
+const streamPipeline = promisify(pipeline);
 
-    try {
-        if (command === 'play') {
-            conn.reply(m.chat, 'Please wait while we fetch the audio...', m, {
-                contextInfo: {
-                    externalAdReply: {
-                        mediaUrl: null,
-                        mediaType: 1,
-                        description: null,
-                        title: 'XLICON-MD', 
-                        body: 'Super Bot WhatsApp',
-                        previewType: 0,
-                        thumbnail: fs.readFileSync("./XLICON.jpg"), 
-                        sourceUrl: 'https://github.com/salmanytofficial/XLICON-V2-MD'
-                    }
-                }
-            });
+let handler = async (message, { conn, command, text, usedPrefix }) => {
+  if (!text) {
+    throw `Use example ${usedPrefix}${command} Jesus take the wheel`;
+  }
+  
+  await message.react('⏳');
 
-            let res = await fetch(`https://violetics.pw/api/media/youtube-play?apikey=beta&query=${text}`);
-            let json = await res.json();
-
-            conn.sendFile(m.chat, json.result.url, 'audio.mp3', null, m, false, { mimetype: 'audio/mp4' });
-        }
-
-        if (command === 'play1') {
-            conn.reply(m.chat, 'Please wait while we fetch the video...', m, {
-                contextInfo: {
-                    externalAdReply: {
-                        mediaUrl: null,
-                        mediaType: 1,
-                        description: null,
-                        title: 'XLICON-MD',
-                        body: 'Super Bot WhatsApp',
-                        previewType: 0,
-                        thumbnail: fs.readFileSync("./XLICON.jpg"), 
-                        sourceUrl: 'https://github.com/salmanytofficial/XLICON-V2-MD'
-                    }
-                }
-            });
-
-            let res = await fetch(`https://violetics.pw/api/media/youtube-play?apikey=beta&query=${text}`);
-            let json = await res.json();
-
-            conn.sendFile(m.chat, json.result.url, 'video.mp4', 'Here is the video!', m);
-        }
-    } catch (e) {
-        await conn.reply(m.chat, 'There was an error fetching the media. Please try again later.', m);
-        console.log(e);
+  try {
+    const query = encodeURIComponent(text);
+    const searchResponse = await axios.get(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${query}`);
+    const video = searchResponse.data.results[0];
+    
+    if (!video) {
+      throw "Video Not Found, Try Another Title";
     }
+
+    const { title, thumbnail, duration, views, uploaded, url } = video;
+    const caption = `⬡▸  ••๑⋯ ⬡▸  Y O U T U B E ⬡▸  ⋯⋅๑•• ✼\n\n  ⬡▸  Title: ${title}\n\n  ⬡▸  Duration: ${duration}\n\n  ⬡▸  Views: ${views}\n\n  ⬡▸  Upload: ${uploaded}\n\n  ⬡▸  Link: ${url}\n\n⊱─━━━━⊱༻XLICON●v2༺⊰━━━━─⬡▸ `;
+    
+    await conn.sendMessage(message.chat, {
+      image: { url: thumbnail },
+      caption: caption,
+      footer: 'xlicon',
+    }, {
+      quoted: message
+    });
+
+    const audioStream = youtubedl(url, {
+      filter: 'audioonly',
+      quality: 'highestaudio',
+    });
+
+    const tempDir = os.tmpdir();
+    const filePath = `${tempDir}/${title}.mp3`;
+    const fileStream = fs.createWriteStream(filePath);
+
+    await streamPipeline(audioStream, fileStream);
+
+    const audioMessage = {
+      audio: { url: filePath },
+      mimetype: 'audio/mpeg',
+      ptt: false,
+      waveform: [100, 0, 0, 0, 0, 0, 100],
+      fileName: `${title}.mp3`,
+      contextInfo: {
+        externalAdReply: {
+          showAdAttribution: true,
+          mediaType: 2,
+          mediaUrl: url,
+          title: title,
+          body: 'HERE IS YOUR SONG MADE BY XLICON-v2',
+          sourceUrl: url,
+          thumbnail: await (await conn.getFile(thumbnail)).data,
+        },
+      },
+    };
+
+    await conn.sendMessage(message.chat, audioMessage, { quoted: message });
+
+    fs.unlink(filePath, err => {
+      if (err) {
+        console.error(`Failed to delete audio file: ${err}`);
+      } else {
+        console.log(`Deleted audio file: ${filePath}`);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    throw "An error occurred while searching for YouTube videos.";
+  }
 };
 
-handler.help = ['play', 'play1'].map(v => v + ' <song name or URL>');
+handler.help = ["play"].map(v => v + " <query>");
 handler.tags = ['downloader'];
-handler.command = ['play', 'play1'];
+handler.command = /^play$/i;
+handler.exp = 0;
 
 export default handler;
