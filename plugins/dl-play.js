f5cdc3ca689cd42bb1321617af6be0edd35e0874
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-11-08 21:09:14 +0000

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,86 +1,29 @@
-import youtubedl from 'youtubedl-core';
-import 'youtube-yts';
-import fs from 'fs';
-import { pipeline } from 'stream';
-import { promisify } from 'util';
-import os from 'os';
 import axios from 'axios';
 
-const streamPipeline = promisify(pipeline);
-
-let handler = async (message, { conn, command, text, usedPrefix }) => {
+let handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!text) {
-    throw `Use example ${usedPrefix}${command} Jesus take the wheel`;
+    throw `Please provide a search query. Example: ${usedPrefix + command} <query>`;
   }
-  
-  await message.react('⏳');
-
+  await m.react('⏳');
   try {
     const query = encodeURIComponent(text);
-    const searchResponse = await axios.get(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${query}`);
-    const video = searchResponse.data.results[0];
-    
-    if (!video) {
-      throw "Video Not Found, Try Another Title";
-    }
+    const title = text; 
 
-    const { title, thumbnail, duration, views, uploaded, url } = video;
-    const caption = `⬡▸  ••๑⋯ ⬡▸  Y O U T U B E ⬡▸  ⋯⋅๑•• ✼\n\n  ⬡▸  Title: ${title}\n\n  ⬡▸  Duration: ${duration}\n\n  ⬡▸  Views: ${views}\n\n  ⬡▸  Upload: ${uploaded}\n\n  ⬡▸  Link: ${url}\n\n⊱─━━━━⊱༻XLICON●v2༺⊰━━━━─⬡▸ `;
-    
-    await conn.sendMessage(message.chat, {
-      image: { url: thumbnail },
-      caption: caption,
-      footer: 'xlicon',
-    }, {
-      quoted: message
-    });
-
-    const audioStream = youtubedl(url, {
-      filter: 'audioonly',
-      quality: 'highestaudio',
-    });
-
-    const tempDir = os.tmpdir();
-    const filePath = `${tempDir}/${title}.mp3`;
-    const fileStream = fs.createWriteStream(filePath);
-
-    await streamPipeline(audioStream, fileStream);
-
-    const audioMessage = {
-      audio: { url: filePath },
+    var aud = `https://ironman.koyeb.app/ironman/dl/yta?url=${query}`;
+    await conn.sendMessage(m.chat, {
+      audio: { url: aud },
       mimetype: 'audio/mpeg',
       ptt: false,
-      waveform: [100, 0, 0, 0, 0, 0, 100],
-      fileName: `${title}.mp3`,
-      contextInfo: {
-        externalAdReply: {
-          showAdAttribution: true,
-          mediaType: 2,
-          mediaUrl: url,
-          title: title,
-          body: 'HERE IS YOUR SONG MADE BY XLICON-v2',
-          sourceUrl: url,
-          thumbnail: await (await conn.getFile(thumbnail)).data,
-        },
-      },
-    };
+      fileName: title,
+    }, { quoted: m });
 
-    await conn.sendMessage(message.chat, audioMessage, { quoted: message });
-
-    fs.unlink(filePath, err => {
-      if (err) {
-        console.error(`Failed to delete audio file: ${err}`);
-      } else {
-        console.log(`Deleted audio file: ${filePath}`);
-      }
-    });
   } catch (error) {
     console.error(error);
-    throw "An error occurred while searching for YouTube videos.";
+    throw 'An error occurred while searching for the YouTube video or fetching the audio.';
   }
 };
 
-handler.help = ["play"].map(v => v + " <query>");
+handler.help = ['play'].map(command => command + ' <query>');
 handler.tags = ['downloader'];
 handler.command = /^play$/i;
 handler.exp = 0;
