28f6fb29b29ca4ba528a04d143473c4ce881717c
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-12-16 15:38:52 +0000

    Update img-gimage.js

--- plugins/img-gimage.js
+++ plugins/img-gimage.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+
 let handler = async (m) => {
 	let message = m.quoted ? m.quoted : m;
 	let text = (message.text || '').trim();
@@ -10,27 +11,28 @@ let handler = async (m) => {
 		let response = await fetch(`https://ironman.koyeb.app/ironman/wallpaper/wpcom?name=${encodeURIComponent(text)}`);
 		var res = await response.json();
 		await m.react('✅');
+		
 		let count = 0;
 		for (const i of res) {
-		    if (count === 5) break;
-    		await conn.sendMessage(m.chat, {
-    			image: {
-    				url: i.image
-    			},
-    			caption: i.title
-    		}, {
-    			quoted: m
-    		});
-    		count++
+			if (count === 5) break;
+			if (i.image) {
+				await conn.sendMessage(m.chat, {
+					image: { url: i.image },
+					caption: i.title
+				}, { quoted: m });
+				count++;
+			}
 		}
 	}
 	catch (error) {
-     console.log(error);
+		console.log(error);
 		await m.reply(error);
 		throw error;
 	}
 };
+
 handler.help = ['img'];
 handler.tags = ['ai'];
 handler.command = /^(img)$/i;
+
 export default handler;
