0960a21dcb4dbadfcc0ac355eccc3a24d6bae333
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-12-26 14:33:43 -0800

    Update gemini.js

--- plugins/gemini.js
+++ plugins/gemini.js
@@ -1,25 +1,22 @@
-import {GoogleGenerativeAI} from '@google/generative-ai'
-
-const genAI = new GoogleGenerativeAI('AIzaSyBWozNQdyPr6q5D7U1Izfl3BArjnNfwGuA');
-
-
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   try {
     if (!text) throw `uhm.. what do you want to say?`
     m.react('🤖')
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-    const prompt = text
-
-    const result = await model.generateContent(prompt);
-    const response = result.response;
-    const textt = response.text();
-    m.reply(textt)
+    const response = await fetch(`https://bk9.fun/ai/gemini?q=${encodeURIComponent(text)}`);
+    const result = await response.json();
+    
+    if (result.status) {
+      m.reply(result.BK9);
+    } else {
+      m.reply('Oops! Something went wrong. , we are trying had to fix it asap');
+    }
   } catch (error) {
     console.error(error);
     m.reply('Oops! Something went wrong. , we are trying had to fix it asap');
   }
 }
+
 handler.help = ['gemini <text>']
 handler.tags = ['tools']
 handler.command = /^(gemini)$/i
