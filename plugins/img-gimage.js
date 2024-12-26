a9f3b0b6c082ec1295f21c61035527d887ac848e
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-12-26 15:02:40 -0800

    Update img-gimage.js

--- plugins/img-gimage.js
+++ plugins/img-gimage.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-let handler = async (m) => {
+let handler = async (m, { conn }) => {
     let message = m.quoted ? m.quoted : m;
     let text = (message.text || '').trim();
     if (!text) {
@@ -12,10 +12,11 @@ let handler = async (m) => {
         let res = await response.json();
         await m.react('✅');
         
-        if (res && res.length > 0 && res[0].image) {
+        if (res && res.length > 0) {
+            let randomResult = res[Math.floor(Math.random() * res.length)];
             await conn.sendMessage(m.chat, {
-                image: { url: res[0].image },
-                caption: res[0].title
+                image: { url: randomResult.image },
+                caption: randomResult.title || "No title provided."
             }, { quoted: m });
         } else {
             throw "❌ No image found for the given text.";
