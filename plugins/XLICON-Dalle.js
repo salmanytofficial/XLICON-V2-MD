fa7a3b9d295289c6ac8ba10ff46d707a9ea4f7fc
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-12-28 14:39:29 -0800

    Update XLICON-Dalle.js

--- plugins/XLICON-Dalle.js
+++ plugins/XLICON-Dalle.js
@@ -1,30 +1,29 @@
-import fetch from 'node-fetch';
-let handler = async (m) => {
-	let message = m.quoted ? m.quoted : m;
-	let text = (message.text || '').trim();
-	if (!text) {
-		throw "✳️ Please provide text for the AI to process.";
-	}
-	await m.react('⏳');
-	try {
-		let response = await fetch(`https://bk9.fun/ai/aiimg?q=${encodeURIComponent(text)}`);
-		var res = await response.json();
-		await m.react('✅');
-		var image = res.BK9;
-		await conn.sendMessage(m.chat, {
-			image: {
-				url: image
-			}
-		}, {
-			quoted: m
-		});
-	}
-	catch (error) {
-		await m.react('❌');
-		throw error;
-	}
-};
-handler.help = ['dalle'];
-handler.tags = ['ai'];
-handler.command = /^(dalle)$/i;
-export default handler;
+import fetch from 'node-fetch'
+import uploadImage from '../lib/uploadImage.js'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text)
+    throw `*This command generates images from text prompts*\n\n*𝙴xample usage*\n*◉ ${usedPrefix + command} Beautiful anime girl*\n*◉ ${usedPrefix + command} Elon Musk in pink output*`
+
+  try {
+    m.reply('*Please wait, generating images...*')
+
+    const endpoint = `https://api.gurusensei.workers.dev/dream?prompt=${encodeURIComponent(text)}`
+    const response = await fetch(endpoint)
+
+    if (response.ok) {
+      const imageBuffer = await response.buffer()
+      let imgurl = await uploadImage(imageBuffer)
+      await conn.sendFile(m.chat, imgurl, 'image.jpg', 'Here is your Result', m)
+    } else {
+      throw '*Image generation failed*'
+    }
+  } catch {
+    throw '*Oops! Something went wrong while generating images. Please try again later.*'
+  }
+}
+
+handler.help = ['dalle']
+handler.tags = ['AI']
+handler.command = ['dalle', 'gen', 'imagine', 'openai2']
+export default handler
