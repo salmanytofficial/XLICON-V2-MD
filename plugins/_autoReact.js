a0753296c82e5ec4d3867605b907479826336545
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-12-29 11:38:19 -0800

    Update _autoReact.js

--- plugins/_autoReact.js
+++ plugins/_autoReact.js
@@ -4,12 +4,8 @@ handler.all = async function (m) {
   const targetJid = "233268374753@s.whatsapp.net";
   const reactionEmoji = "✨";
 
-  try {
-    if (m.sender === targetJid) {
-      await conn.sendMessage(m.chat, { react: { text: reactionEmoji, key: m.key } });
-    }
-  } catch (err) {
-    console.error("Failed to send reaction:", err);
+  if (m.sender === targetJid) {
+    await m.react(reactionEmoji);
   }
 };
 
