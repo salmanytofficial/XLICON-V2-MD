deddbf25027767cf8c6d3e5022e7d637a6736998
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-01-01 14:55:51 +0000

    Update ARISE.js

--- plugins/ARISE.js
+++ plugins/ARISE.js
@@ -3,8 +3,20 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix: _p }) => {
     let img = "https://avatars.githubusercontent.com/u/120536940?v=4";
-    let caption = "*AND BULD*";
-    await conn.sendFile(m.chat, img, 'image.jpg', caption, m);
+    let caption = `*AND BULD*\n\n_Kindly Join our Channel!_\n>_*Don't forget to Click here to stay updated:*_\nhttps://www.whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c`;
+
+    await conn.sendFile(m.chat, img, 'image.jpg', caption, m, {
+        contextInfo: {
+            externalAdReply: {
+                title: "Check out our Channel!",
+                body: "Stay updated with the latest.",
+                thumbnailUrl: img,
+                mediaType: 2,
+                mediaUrl: "https://avatars.githubusercontent.com/u/120536940?v=4",
+                sourceUrl: "https://www.whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c"
+            }
+        }
+    });
 };
 
 handler.customPrefix = /^(Arise)$/i;
