7c33c13f0a89c3e1c293cb528cf96167e3316c51
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-01-01 07:36:28 -0800

    Update ARISE.js

--- plugins/ARISE.js
+++ plugins/ARISE.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   try {
     const img = "https://avatars.githubusercontent.com/u/120536940?v=4";
-    const forwardMessage = `*Here is an amazing image for you!*\n\n_Kindly Join our Channel!_\n>Don't forget to Click here to stay updated:\nhttps://www.whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c`;
+    const forwardMessage = `*Here is an amazing image for you!*\n\n_Kindly Join our Channel!_\n> Don't forget to Click here to stay updated:\nhttps://www.whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c`;
     const url = "https://www.whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c";
     const murl = "https://www.whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c";
     const hash = global.botname;
