7179f116a3f084c3513da1b6bf434541fa8726d9
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-01-09 09:12:42 -0800

    Update Wall-Ronaldo.js

--- plugins/Wall-Ronaldo.js
+++ plugins/Wall-Ronaldo.js
@@ -1,10 +1,38 @@
-import axios from 'axios'
-let handler = async(m, { conn, usedPrefix, command }) => {
-let cristiano = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/CristianoRonaldo.json`)).data  
-let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
-conn.sendFile(m.chat, ronaldo, 'error.jpg', `*siuuuuuuuuuu*`, m)}
-//conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
-handler.help = ['cristianoronaldo', 'cr7']
-handler.tags = ['img']
-handler.command = /^(ronaldo|cr7)$/i
-export default handler
+import axios from 'axios';
+
+const handler = async (m, { conn, usedPrefix, command }) => {
+  try {
+    const response = await axios.get(
+      'https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/CristianoRonaldo.json'
+    );
+    const cristiano = response.data;
+    const ronaldo = cristiano[Math.floor(Math.random() * cristiano.length)];
+
+    const forwardMessage = '*Siiiuuuuuuuuu!*';
+    const hash = '*Powered 𝙗𝙮 𝐱𝐯2 𝐌𝐃*';
+
+    const doc = {
+      image: { url: ronaldo },
+      caption: forwardMessage,
+      contextInfo: {
+        externalAdReply: {
+          title: '❀•°Cristiano Ronaldo°•❀',
+          body: hash,
+          thumbnailUrl: ronaldo,
+          showAdAttribution: true
+        }
+      }
+    };
+    
+    await conn.sendMessage(m.chat, doc, { quoted: m });
+
+  } catch {
+    throw '*Error!*';
+  }
+};
+
+handler.help = ['cristianoronaldo', 'cr7'];
+handler.tags = ['img'];
+handler.command = /^(ronaldo|cr7)$/i;
+
+export default handler;
