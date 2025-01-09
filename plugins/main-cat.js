c9e030e4ad1fbfecb7a220f3faf624e83fa2c529
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-01-09 08:55:35 -0800

    Update main-cat.js

--- plugins/main-cat.js
+++ plugins/main-cat.js
@@ -1,45 +1,29 @@
 import fetch from 'node-fetch';
 
-let handler = async (m, { conn, text }) => {
+const handler = async (m, { conn, text }) => {
   try {
-    let res = await fetch('https://cataas.com/cat');
-    let img = await res.buffer();
-    let caption = `*here is a random cat🐈*`.trim();
-    conn.sendFile(m.chat, img, 'cat.jpg', caption, m);
+    const res = await fetch('https://cataas.com/cat');
+    const img = await res.buffer();
 
-    const str = "Here's an exciting message!";
-    await conn.sendMessage(m.chat, {
-      text: str,
+    const forwardMessage = '🌟Here is a random cat for you!';
+    const hash = "*Powered 𝙗𝙮  𝐱𝐯2 𝐌𝐃*";
+
+    const doc = {
+      image: { url: img },
+      caption: forwardMessage,
       contextInfo: {
         externalAdReply: {
-          title: "Adventure Awaits!",
-          body: "Check out the latest updates.",
-          thumbnailUrl: "https://telegra.ph/file/b1b157e944010efebf1d7.jpg",
-          sourceUrl: "https://www.whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c",
+          title: "❀•°Cat°•❀",
+          body: hash,
+          thumbnailUrl: img,
+          showAdAttribution: true
         }
       }
-    }, { quoted: m });
+    };
 
-    m.react('✅');
+    await conn.sendMessage(m.chat, doc, { quoted: m });
 
-    if (m.quoted && m.quoted.isForwarded) {
-      const forwardInfo = m.quoted.forwardingScore > 0 ? `Forwarded ${m.quoted.forwardingScore} times` : "Recently Forwarded";
-      const forwardMessage = `This message was ${forwardInfo}.\nContent: ${m.quoted.text || "(No Text Content)"}`;
-
-      await conn.sendMessage(m.chat, {
-        text: forwardMessage,
-        contextInfo: {
-          externalAdReply: {
-            title: "Forwarded Message Info",
-            body: "Learn more about this message.",
-            thumbnailUrl: "https://telegra.ph/file/b1b157e944010efebf1d7.jpg",
-            sourceUrl: "https://www.whatsapp.com/channel/0029VaMGgVL3WHTNkhzHik3c",
-          }
-        }
-      }, { quoted: m });
-    }
-  } catch (e) {
-    console.log(e);
+  } catch {
     throw '*Error!*';
   }
 };
