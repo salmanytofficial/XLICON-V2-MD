7ceec230b0e43daf721e74026871c7c74afbec4c
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-01-11 20:09:37 -0800

    Update econ-levelup.js

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -4,7 +4,8 @@ let handler = async (m, { conn }) => {
     let name = conn.getName(m.sender);
     let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.imgur.com/whjlJSf.jpg');
     let user = global.db.data.users[m.sender];
-    let background = 'https://telegra.ph/file/13b0b0f7513b242559f1f.jpg'; // Fixed background URL
+    let background = 'https://telegra.ph/file/13b0b0f7513b242559f1f.jpg'; 
+    let waifuImg = global.hwaifu[Math.floor(Math.random() * global.hwaifu.length)]; 
 
     if (!canLevelUp(user.level, user.exp, global.multiplier)) {
         let { min, xp, max } = xpRange(user.level, global.multiplier);
@@ -20,8 +21,7 @@ Hey there, ${name}! You're not ready to level up just yet. It seems like you nee
 `.trim();
 
         try {
-            let imgg = `https://wecomeapi.onrender.com/rankup-image?username=${encodeURIComponent(name)}&currxp=${user.exp - min}&needxp=${xp}&level=${user.level}&rank=${encodeURIComponent(pp)}&avatar=${encodeURIComponent(pp)}&background=${encodeURIComponent(background)}`;
-            conn.sendFile(m.chat, imgg, 'level.jpg', txt, m);
+            conn.sendFile(m.chat, waifuImg, 'level.jpg', txt, m);
         } catch (e) {
             m.reply(txt);
         }
@@ -38,8 +38,7 @@ Your newfound power will strike fear into the hearts of trolls, and the bots wil
 `.trim();
 
         try {
-            let img = `https://wecomeapi.onrender.com/levelup-image?avatar=${encodeURIComponent(pp)}`;
-            conn.sendFile(m.chat, img, 'levelup.jpg', str, m);
+            conn.sendFile(m.chat, waifuImg, 'levelup.jpg', str, m);
         } catch (e) {
             m.reply(str);
         }
@@ -50,4 +49,4 @@ handler.help = ['levelup'];
 handler.tags = ['economy'];
 handler.command = ['lvl', 'levelup', 'level'];
 
-export default handler
+export default handler;
