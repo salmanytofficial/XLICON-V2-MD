22cd5ab3b864e529aadcde00308a0525299675ef
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-01-18 06:26:26 -0800

    Update logo.js

--- plugins/logo.js
+++ plugins/logo.js
@@ -1,18 +1,20 @@
 import fetch from 'node-fetch'
 
 let logos = {
-    "dragonball": "https://bk9.fun/maker/ephoto-1?text=BK9&url=https://en.ephoto360.com/create-dragon-ball-style-text-effects-online-809.html",
-    "cartoon": "https://bk9.fun/maker/ephoto-1?text=BK9&url=https://en.ephoto360.com/create-a-cartoon-style-graffiti-text-effect-online-668.html",
-    "viettel": "https://bk9.fun/maker/ephoto-1?text=BK9&url=https://en.ephoto360.com/logo-viettel-156.html",
-    "foggyglass": "https://bk9.fun/maker/ephoto-1?text=BK9&url=https://en.ephoto360.com/handwritten-text-on-foggy-glass-online-680.html",
-    "naruto": "https://bk9.fun/maker/ephoto-1?text=BK9&url=https://en.ephoto360.com/naruto-shippuden-logo-style-text-effect-online-808.html",
-    "coffee": "https://bk9.fun/maker/ephoto-1?text=BK9&url=https://en.ephoto360.com/message-coffee-245.html"
+    "dragonball": "https://bk9.fun/maker/ephoto-1?text={text}&url=https://en.ephoto360.com/create-dragon-ball-style-text-effects-online-809.html",
+    "cartoon": "https://bk9.fun/maker/ephoto-1?text={text}&url=https://en.ephoto360.com/create-a-cartoon-style-graffiti-text-effect-online-668.html",
+    "viettel": "https://bk9.fun/maker/ephoto-1?text={text}&url=https://en.ephoto360.com/logo-viettel-156.html",
+    "foggyglass": "https://bk9.fun/maker/ephoto-1?text={text}&url=https://en.ephoto360.com/handwritten-text-on-foggy-glass-online-680.html",
+    "naruto": "https://bk9.fun/maker/ephoto-1?text={text}&url=https://en.ephoto360.com/naruto-shippuden-logo-style-text-effect-online-808.html",
+    "coffee": "https://bk9.fun/maker/ephoto-1?text={text}&url=https://en.ephoto360.com/message-coffee-245.html"
 }
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
-    if (!text) return await conn.sendMessage(m.chat, { text: `Use ${usedPrefix + command} Ab Dev.` })
+    if (!text) return await conn.sendMessage(m.chat, { text: `Use ${usedPrefix + command} <Your Text>` })
     
-    let response = await fetch(logos[command])
+    let url = logos[command].replace("{text}", encodeURIComponent(text))
+
+    let response = await fetch(url)
     let data = await response.json()
     
     if (data.status) {
