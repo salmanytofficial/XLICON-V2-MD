0f403d07917d4f2908583a04287270c646223a57
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-01-22 21:05:56 -0800

    shit

--- statusreact.js
+++
@@ -1,6 +0,0 @@
-async function reactToStatus(m, conn) {
-  if (process.env.statusview && m.key.remoteJid === 'status@broadcast') {
-    await conn.readMessages([m.key])
-    await conn.react(m.key, '👍')
-  }
-}
