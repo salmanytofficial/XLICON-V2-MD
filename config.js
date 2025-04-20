ac31323e3376389f923eb1470662b4ecf33a810a
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-04-20 11:04:17 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,48 +2,31 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 import fs from 'fs'
+import fetch from 'node-fetch'
+import axios from 'axios'
 
-import dotenv from 'dotenv'
-dotenv.config()
-
-const ownervb = process.env.OWNERS || "923184070915;salman;233533763772;Abraham"
-
-
-const ownerlist = ownervb.split(';');
-
-global.owner = [];
-for (let i = 0; i < ownerlist.length; i += 2) {
-    const owner = [
-        ownerlist[i],            
-        ownerlist[i + 1],         
-        true                        
-    ];
-    global.owner.push(owner);
-}
-
-//global.pairingNumber = "" //put your bot number here
+// Global variables
+global.owner = [
+  ['918360234087', 'Ｗ Ｈ Ｉ Ｔ Ｅ ４ ４ ４  ? ?', true], 
+  ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
+  ['233533763772', 'Abraham Dwamena2.0', true],
+  ['233268374753', 'Abraham Dwamena3.0', true]
+] 
 
 global.mods = ['917888755628','923184070915', '233268374753', '923184474176', '233268374753'] 
-global.prems = ['917888755628','923184070915', '233268374753', '923184474176', '233268374753'] 
-global.allowed = ['917888755628','923184070915', '233268374753', '923184474176', '233268374753'] 
-global.keysZens = ['917888755628','923184070915', '233268374753', '923184474176', '233268374753'] 
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
-global.keysxteammm = [
-  '29d4b59a4aa687ca',
-  '5LTV57azwaid7dXfz5fzJu',
-  'cb15ed422c71a2fb',
-  '5bd33b276d41d6b4',
-  'HIRO',
-  'kurrxd09',
-  'ebb6251cc00f9c63',
-]
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
+global.prems = ['923184070915', '94789481495', '918360234087', '233268374753']
+global.allowed = ['923184070915', '233268374753', '918360234087', '233268374753']
+global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
+global.keysxxx = global.keysZens[Math.floor(global.keysZens.length * Math.random())]
+global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
+global.keysxteam = global.keysxteammm[Math.floor(global.keysxteammm.length * Math.random())]
 global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
+global.keysneoxr = global.keysneoxrrr[Math.floor(global.keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['GataDios']
+global.beta = 'mLxstUwm'
 
 global.APIs = {
-  xteam: 'https://api.xteam.xyz',
+  xteam: 'https://api.xteam.xyz', 
   dzx: 'https://api.dhamzxploit.my.id',
   lol: 'https://api.lolhuman.xyz',
   violetics: 'https://violetics.pw',
@@ -53,17 +36,19 @@ global.APIs = {
   akuari2: 'https://apimu.my.id',
   nrtm: 'https://fg-nrtm.ddns.net',
   bg: 'http://bochil.ddns.net',
-  fgmods: 'https://api-fgmods.ddns.net',
-}
-global.APIKeys = {
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
-  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,
-  'https://violetics.pw': 'beta',
-  'https://zenzapis.xyz': `${keysxxx}`,
-  'https://api-fgmods.ddns.net': 'fg-dylux',
+  fgmods: 'https://api-fgmods.ddns.net'
 }
 
+global.APIKeys = { 
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+  'https://api.neoxr.my.id': global.keysneoxr,
+  'https://violetics.pw': 'beta',
+  'https://zenzapis.xyz': global.keysxxx, 
+  'https://api-fgmods.ddns.net': 'fg-dylux'
+}
+
+// Sticker WM
 global.botname = '𝘟𝘭𝘪𝘤𝘰𝘯 𝘝𝘰𝘭 2'
 global.premium = 'true'
 global.packname = '𝙏𝙚𝙖𝙢 𝙓𝙇𝙄𝘾𝙊𝙉' 
@@ -82,9 +67,9 @@ global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✔️'
 global.error = '❌' 
-global.xmoji = '🔥'
+global.xmoji = '🔥' 
 
-global.multiplier = 69
+global.multiplier = 69 
 global.maxwarn = '3'
 
 let file = fileURLToPath(import.meta.url)
